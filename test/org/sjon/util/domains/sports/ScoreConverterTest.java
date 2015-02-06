7f3e6ad0aaebc8b2bf77174f67ce6d9c9d2be2b5
Andreas Tasoulas <andreas@pubsoccermanager.com>
2015-02-06 15:51:48 +0200

    Converting scores from a file

--- test/org/sjon/util/domains/sports/ScoreConverterTest.java
+++ test/org/sjon/util/domains/sports/ScoreConverterTest.java
@@ -2,6 +2,8 @@ package org.sjon.util.domains.sports;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,6 +11,8 @@ public class ScoreConverterTest {
 	
 	private String score1;
 	private String score2;
+	
+	private File scoresFile;
 	
 	@Before
 	public void setUp() {
@@ -20,5 +24,14 @@ public class ScoreConverterTest {
 	public void testSingleScoreConversion() {
 		assertEquals("{Young Boys,Basel,0,1}", ScoreConverter.toSjon(score1));
 		assertEquals("{Young Boys,Basel,0,1}", ScoreConverter.toSjon(score2));
+	}
+	
+	@Test
+	public void testScoreConversionFromFile() {
+		
+		this.scoresFile = new File("resources/scores.txt");
+		
+		// Just a rough check
+		System.out.println(ScoreConverter.toSjon(this.scoresFile));
 	}
 }
